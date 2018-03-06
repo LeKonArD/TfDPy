1971158fd51b18596020fe2cc1eedaa488c02242
LeKonArD <leonardkonle@gmail.com>
2018-03-06 20:41:30 +0100

    Formatting pep and add spacy models

--- lib/utils.py
+++ lib/utils.py
@@ -9,25 +9,26 @@ Contents
 ********
 
 """
-#################### CORE Filehandling #####################
-import nltk
+# CORE File handling
 import os
+import spacy as sp
 import pandas as pd
-import numpy as np
+
 
 def collect_files_from_dir(corpus_folder, file_ending):
 
-    # create single-column pandas dataframe containing filenames from dir with certain ending
+    # create single-column pandas DataFrame containing Paths from dir with certain ending
 
     files = list()
-    for path, subdirs, filenames in os.walk(corpus_folder):
-        for filename in filenames:
+    for path, sub_dirs, file_names in os.walk(corpus_folder):
+        for filename in file_names:
             if filename.endswith(file_ending):
                 files.append(os.path.join(path, filename))
     files.sort()
-    corpus_df = pd.DataFrame(files, columns=['filepath'])
+    corpus_df = pd.DataFrame(files, columns=['file_path'])
 
     return corpus_df
+
 
 def add_categories(corpus_df):
 
@@ -37,65 +38,66 @@ def add_categories(corpus_df):
 
     return corpus_df
 
+
 def add_text(corpus_df):
 
-    corpus_df["text"] = corpus_df["filepath"].apply(lambda x:read_save(x))
+    corpus_df["text"] = corpus_df["file_path"].apply(lambda x: read_save(x))
 
     return corpus_df
 
+
 def read_save(f_path):
 
-
-    with open(f_path,"r") as file:
+    with open(f_path, "r") as file:
         text = file.read()
 
     return text
 
 
-################ Preprocessing text #################
+# preprocessing text
 def to_sentences(corpus_df):
 
-    sent_detector = nltk.data.load('tokenizers/punkt/german.pickle')
-    corpus_df["text"] = corpus_df["text"].apply(lambda x: sent_detector.tokenize(x))
-
     return corpus_df
+
 
 def to_tokens(x):
 
+    return x
+
+
+def to_chunks(x, chunk_size):
+
+    chunk_size += 1
 
     return x
 
-def to_chunks(x,chunk_size):
-
-
-    return x
 
 def to_chars(corpus_df):
 
-    corpus_df["text"]=corpus_df["text"].apply(lambda x: list(x))
-
-    return corpus_df
-
-def to_ngrams(corpus_df,gram_size):
-
-    corpus_df["text"] = corpus_df["text"].apply(lambda x: nltk.ngrams(x,gram_size))
+    corpus_df["text"] = corpus_df["text"].apply(lambda x: list(x))
 
     return corpus_df
 
 
+def to_ngrams(corpus_df, gram_size):
+
+    gram_size += 1
+
+    return corpus_df
 
 
+def initial_spacy_step(corpus_df, lang):
+
+    nlp = sp.load(lang)
+    corpus_df["NLP"] = corpus_df["text"].apply(lambda x: nlp(x))
+
+    return corpus_df
 
 
+# Tests
 
-
-
-################ Tests
-corpus = collect_files_from_dir("/home/leo/Documents/schemaliteratur_DS/test_HA","xml")
+corpus = collect_files_from_dir("/home/leo/Documents/schemaliteratur_DS/test_HA", "xml")
 corpus = add_categories(corpus)
 corpus = add_text(corpus)
-corpus = to_tokens(corpus)
-corpus = to_ngrams(corpus,3)
-gram = corpus["text"][1]
-for g in gram:
-    print(g)
+corpus = initial_spacy_step(corpus, "de")
+print(corpus["NLP"].iloc[1])
